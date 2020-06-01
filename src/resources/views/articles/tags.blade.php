3ebd5d1d2f67e39e479d27c97164419e152d0aa8
nagamoridaiki <no.swea1017@gmail.com>
2020-06-01 20:55:38 +0900

    タグ何件の表示を消した

--- src/resources/views/articles/tags.blade.php
+++ src/resources/views/articles/tags.blade.php
@@ -3,7 +3,7 @@
     <div class='tags-backcolor'>
     @foreach($tags as $tag)
         @if($loop->first)
-        <p>合計{{ $tag->count_tag }}件</p>
+        
         @endif
         <p><a href="{{ route('tags.show', ['name' => $tag->name]) }}" class="font-weight-bold border p-1 text-muted">
             {{ $tag->hashtag }}</a></p>
--- src/resources/views/tags/show.blade.php
+++ src/resources/views/tags/show.blade.php
@@ -9,7 +9,6 @@
       <div class="card-body">
         <h2 class="h4 card-title m-0">{{ $tag->hashtag }}</h2>
         <div class="card-text text-right">
-          {{ $tag->articles->count() }}件
         </div>
       </div>
     </div>
